d7079fb868af203e06cfc0b3282148f482e678a4
raza1315 <razasfs2003@gmail.com>
2025-03-01 22:55:26 +0530

    COmments

--- test.sh
+++ test.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-#The above line is call hashbang/shebang/shabang
+#The above line is call hashbang/shebang/shabang (tells the OS which interpreter to use)
 
 echo "Hey"
 echo "This is a shell script"
+
+# single line comment
+
+<< comment
+this 
+is 
+a multi line comment
+comment
+
