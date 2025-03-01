227b0f80e248f3ba81d1768059d54c877e5cb570
raza1315 <razasfs2003@gmail.com>
2025-03-01 22:59:51 +0530

    file permissions and variables

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #The above line is call hashbang/shebang/shabang (tells the OS which interpreter to use)
+# Also make the files executable chmod 764 (rwx rw- r--)
 
 echo "Hey"
 echo "This is a shell script"
---
+++ variables.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+name="raza"
+echo "This is $name" 
