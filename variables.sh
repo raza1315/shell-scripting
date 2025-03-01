329441290ae5298b5d4734b7d985d32b4e8d7527
raza1315 <razasfs2003@gmail.com>
2025-03-01 23:04:27 +0530

    date

--- variables.sh
+++ variables.sh
@@ -2,3 +2,11 @@
 
 name="raza"
 echo "This is $name" 
+
+# date is a shell command like echo but to use it like a variable enclose it like this $(command)
+
+#First one is direct command and second is using variable
+date
+echo "Date is : $(date)"
+
+
