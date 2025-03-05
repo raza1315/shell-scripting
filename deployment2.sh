c0ec41d7943244afd11862211b07925f5d301590
raza1315 <razasfs2003@gmail.com>
2025-03-05 23:15:43 +0530

    docker compose deployment done

--- deployment2.sh
+++ deployment2.sh
@@ -9,11 +9,23 @@ git clone https://github.com/raza1315/shell-scripting
 test(){
 pwd
 }
-build_image(){
+build_and_deploy(){
 echo "navigating to the shellDeployment directory..."
 cd shellDeployment
 echo "building the image using docker-compose after docker-compose down to down all the services(containers if running)"
 docker compose down
 docker compose up -d --build 
-
+echo "deployment successful!"
 }
+
+if ! clone_code 
+ then
+   echo "Repo Already exists..." 
+fi
+
+test
+
+if ! build_and_deploy  
+ then
+  echo "some error in docker..." 
+fi
